8ef1523cdeeff6546d7211ab3c71637ad4468cd1
Isaac Harasty <jharasty0101@gmail.com>
2020-11-16 11:32:10 -0500

    miscelaneous fixes for misc people

--- Website_Python_Code/flask_website/routes.py
+++ Website_Python_Code/flask_website/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for, flash, redirect, Response, request
 from matplotlib.figure import Figure
 from matplotlib.pyplot import figure
 '''
+import datetime
 import io
 import base64
 import json
@@ -13,9 +14,81 @@ from flask_login import login_user, current_user, logout_user, login_required, U
 from pprint import pprint
 
 class User(UserMixin):
+
+    def initialize_user_data(self):
+
+        # Getting Sensors for Account
+        data = {}
+
+        data["email"] = self.email
+        data["payment_tier"] = db.accounts.get_status_by_id(self.id)[0]
+        data["sensor_data"] = dict()
+        curr_user_sensors = db.sensors.get_all_sensors(self.id)
+
+        curr_user_groups = {}
+
+        # find all groups for current user
+        for sensor in curr_user_sensors:
+
+            curr_group = db.sensors.get_sensor_info(sensor)[0][6]
+
+            if curr_group != None:
+                if curr_group not in curr_user_groups:
+
+                    temp_list = []
+                    temp_list.append(sensor)
+                    curr_user_groups[curr_group] = temp_list
+
+                else:
+
+                    curr_user_groups[curr_group].append(sensor)
+            else:
+                if "No Group" not in curr_user_groups:
+
+                    temp_list = []
+                    temp_list.append(sensor)
+                    curr_user_groups["No Group"] = temp_list
+
+                else:
+
+                    curr_user_groups["No Group"].append(sensor)
+
+        for group in curr_user_groups:
+
+            '''[(535, '100', 10, 'norm', 'water_tower1', 60, None)]'''
+            group_list = curr_user_groups[group]
+            data["sensor_data"][group] = {}
+
+            for sensor in group_list:
+
+                curr_sensor = {}
+                sensor_data = db.sensors.get_sensor_info(sensor)[0]
+
+                curr_sensor["name"] = sensor_data[4]
+                curr_sensor["x_vals"] = []
+                curr_sensor["y_vals"] = []
+                curr_sensor["bat_level"] = db.sensor_readings.get_sensor_battery(sensor)
+
+                sensor_values = db.sensor_readings.get_sensor_data_points(sensor)
+                counter = 0
+
+                for data_point in sensor_values:
+                    '''
+                    curr_sensor["x_vals"] = data_point[5]
+                    '''
+                    curr_sensor["x_vals"].append(counter)
+                    curr_sensor["y_vals"].append(data_point[3])
+                    counter = counter + 1
+
+                data["sensor_data"][group][sensor] = curr_sensor
+
+        return data
+
     def __init__(self, userID):
         self.id = userID
         self.email = db.accounts.get_email_by_id(userID)[0]
+        self.user_data = self.initialize_user_data()
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -53,164 +126,7 @@ def login():
 @login_required
 def home():
 
-    '''
-    STRUCTURE FOR user data
-
-    {   "user_name": "__USERNAME__"
-        "payment_tier": "__0/1__"
-        "sensor_data": [
-            "sensor_grouping": "__GROUP1__"
-            "group_data": [
-                "sensor_id":__SENSID__
-                "sensor_name":__SENSNAME__
-                "x_vals":[__READINGTIMES__]
-                "y_vals":[__SENSORREADS__]
-                "bat_level":__BATLEV__
-                ,
-                .
-                .
-                ]
-            ,
-            "sensor_grouping": "__GROUP2__"
-            group_data": [
-                "sensor_id":__SENSID__
-                "sensor_name":__SENSNAME__
-                "x_vals":[__READINGTIMES__]
-                "y_vals":[__SENSORREADS__]
-                "bat_level":__BATLEV__
-                ,
-                .
-                .
-                ]
-            ]
-    }
-    '''
-
-
-
-    #Getting Sensors for Account
-    user_data = {}
-
-    user_data["email"] = current_user.email
-    user_data["payment_tier"] = db.accounts.get_status_by_id(current_user.id)[0]
-    user_data["sensor_data"] = dict()
-    curr_user_sensors = db.sensors.get_all_sensors(current_user.id)
-
-    curr_user_groups = {}
-
-    # find all groups for current user
-    for sensor in curr_user_sensors:
-
-        curr_group = db.sensors.get_sensor_info(sensor)[0][6]
-
-        if curr_group != None:
-            if curr_group not in curr_user_groups:
-
-                temp_list = []
-                temp_list.append(sensor)
-                curr_user_groups[curr_group] = temp_list
-
-            else:
-
-                curr_user_groups[curr_group].append(sensor)
-        else:
-            if "No Group" not in curr_user_groups:
-
-                temp_list = []
-                temp_list.append(sensor)
-                curr_user_groups["No Group"] = temp_list
-
-            else:
-
-                curr_user_groups["No Group"].append(sensor)
-
-    print(curr_user_sensors)
-    print(curr_user_groups)
-
-    '''{"user_name": "__USERNAME__"
-        "payment_tier": "__0/1__"
-        "sensor_data": {
-                        "__GROUPNAME__": {
-                                        "sensor_id": {
-                                        "sensor_name": __SENSNAME__
-                                        "x_vals": [__READINGTIMES__]
-                                        "y_vals": [__SENSORREADS__]
-                                        "bat_level": __BATLEV__
-                                                    
-            
-    '''
-    pprint(user_data)
-    for group in curr_user_groups:
-
-        '''[(535, '100', 10, 'norm', 'water_tower1', 60, None)]'''
-        print(curr_user_groups)
-        print(group)
-        group_list = curr_user_groups[group]
-        user_data["sensor_data"][group] = {}
-
-        for sensor in group_list:
-
-            curr_sensor = {}
-            print(sensor)
-            print(group)
-            sensor_data = db.sensors.get_sensor_info(sensor)[0]
-
-            pprint(user_data)
-
-            curr_sensor["name"] = sensor_data[4]
-            curr_sensor["x_vals"] = []
-            curr_sensor["y_vals"] = []
-            curr_sensor["bat_level"] = db.sensor_readings.get_sensor_battery(sensor)
-
-            sensor_values = db.sensor_readings.get_sensor_data_points(sensor)
-            counter = 0
-
-            for data_point in sensor_values:
-
-                '''
-                curr_sensor["x_vals"] = data_point[5]
-                '''
-                curr_sensor["x_vals"].append(counter)
-                curr_sensor["y_vals"].append(data_point[3])
-                counter = counter + 1
-
-
-            pprint(curr_sensor)
-            pprint(user_data)
-
-
-            user_data["sensor_data"][group][sensor] = curr_sensor
-        
-    
-    pprint(user_data)
-    return render_template('home.html', account_info=user_data)
-
-
-'''fig = Figure()
-plt = fig.add_subplot(1, 1, 1)
-
-plt.set_ylim(0, 100)
-plt.set_xlim(0, len(curr_sensor["x_vals"]))
-# naming the x axis
-plt.set_xlabel('Reading Times')
-# naming the y axis
-plt.set_ylabel('Sensor Levels')
-
-# giving a title to my graph
-plt.set_title('Sensor data for %s' % curr_sensor["name"])
-
-plt.plot(curr_sensor["x_vals"], curr_sensor["y_vals"], color='green', linestyle='dashed',
-         linewidth=3, marker='o', markerfacecolor='blue', markersize=12)
-
-# Convert plot to PNG image
-pngImage = io.BytesIO()
-FigureCanvas(fig).print_png(pngImage)
-
-pngImageB64String = "data:image/png;base64,"
-pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-
-curr_sensor["image"] = pngImageB64String'''
-
+    return render_template('home.html', account_info=current_user.user_data)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -239,10 +155,19 @@ def register():
 # SAMPLE
 @app.route("/sensor", methods=['POST'])
 def sensor():
-    
+
+    data_string = "Recieved post at: %s\n" % datetime.datetime.now()
+    print(data_string)
+    data_string = data_string + str(request.json) + "\n"
+
+    with open('./flask_website/records.txt', 'a') as f:
+        f.write(data_string)
+
+
+    '''
     sensor_msg = request.json
     db.sensor_readings.add_reading_no_time(sensor_msg["Sensor ID"], sensor_msg["Liquid %"], sensor_msg["Battery %"], 0)
-    
+    '''
     return "OK"
 
 @app.route("/account", methods=['GET', 'POST'])
@@ -279,3 +204,28 @@ def settings():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+'''fig = Figure()
+plt = fig.add_subplot(1, 1, 1)
+
+plt.set_ylim(0, 100)
+plt.set_xlim(0, len(curr_sensor["x_vals"]))
+# naming the x axis
+plt.set_xlabel('Reading Times')
+# naming the y axis
+plt.set_ylabel('Sensor Levels')
+
+# giving a title to my graph
+plt.set_title('Sensor data for %s' % curr_sensor["name"])
+
+plt.plot(curr_sensor["x_vals"], curr_sensor["y_vals"], color='green', linestyle='dashed',
+         linewidth=3, marker='o', markerfacecolor='blue', markersize=12)
+
+# Convert plot to PNG image
+pngImage = io.BytesIO()
+FigureCanvas(fig).print_png(pngImage)
+
+pngImageB64String = "data:image/png;base64,"
+pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+curr_sensor["image"] = pngImageB64String'''
