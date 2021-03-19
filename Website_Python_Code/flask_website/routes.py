ad8fc19076712441346a49c773575805f441fc03
nanderson870 <nicholasanderson940@yahoo.com>
2021-03-19 12:10:14 -0400

    4 data point mode

--- Website_Python_Code/flask_website/routes.py
+++ Website_Python_Code/flask_website/routes.py
@@ -97,10 +97,10 @@ class User(UserMixin):
                     curr_sensor["y_vals"].append(data_point[3])
                     counter = counter + 1
 
-                if len(curr_sensor['y_vals']) > 80:
+                if len(curr_sensor['y_vals']) > 4:
                     num_readings = len(curr_sensor['y_vals'])
-                    curr_sensor['x_vals'] = curr_sensor['x_vals'][num_readings - 80:]
-                    curr_sensor['y_vals'] = curr_sensor['y_vals'][num_readings - 80:]
+                    curr_sensor['x_vals'] = curr_sensor['x_vals'][num_readings - 4:]
+                    curr_sensor['y_vals'] = curr_sensor['y_vals'][num_readings - 4:]
 
                 data["sensor_data"][group][sensor] = curr_sensor
 
