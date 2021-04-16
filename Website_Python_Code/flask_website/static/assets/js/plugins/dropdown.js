af4364195bd01fdb2d30b9b63dddef50ffedb978
Brian <dang.brian10@gmail.com>
2021-04-15 22:39:06 -0400

    Made login text boxs' adapt to webpage's size and fixed registration page design

--- Website_Python_Code/flask_website/static/assets/js/plugins/dropdown.js
+++ Website_Python_Code/flask_website/static/assets/js/plugins/dropdown.js
@@ -3,6 +3,10 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
